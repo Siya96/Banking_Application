0283b465dc3c2fa73d4242916cf21d996568a410
Siya96 <74293233+Siya96@users.noreply.github.com>
2024-01-09 12:40:42 +0100

    Assert return type to Responseentities instead of wildcard is done (#10)
    Co-authored-by: Siya96 <siyabend.revend@gmail.com>

--- src/main/java/com/safeBankAB/safebankapp/model/payment/Transactions.java
+++ src/main/java/com/safeBankAB/safebankapp/model/payment/Transactions.java
@@ -1,4 +1,10 @@
 package com.safeBankAB.safebankapp.model.payment;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Transactions {
+    @Id
+    private long id;
 }
