3f991c6bb0e6bb2f7fe967c99d8504b237f4978c
Siya96 <74293233+Siya96@users.noreply.github.com>
2024-02-26 20:14:05 +0100

    Constant class changed to final and private constructor added to prevent any instantiation (#13)
    Co-authored-by: Siya96 <siyabend.revend@gmail.com>

--- src/main/java/com/safeBankAB/safebankapp/utilities/Constants.java
+++ src/main/java/com/safeBankAB/safebankapp/utilities/Constants.java
@@ -1,7 +1,8 @@
 package com.safeBankAB.safebankapp.utilities;
 
-public class Constants {
+public final class Constants {
 
+    private Constants(){}
     public static final String USER_ALREADY_EXISTS = "Failed to create user";
     public static final String USER_DO_NOT_EXIST = "User does not exits";
 
@@ -10,12 +11,6 @@ public class Constants {
     public static final String SUCCESSFULLY_CREATED_USER_AND_ACCOUNT = "Successfully created user and account";
 
     public static final String FAILED_INPUT_VALIDATION = "Unsuccessfully created user and account";
-
-    public static final int AES_128 = 128;
-
-    public static final int AES_192 = 192;
-    public static final int AES_256 = 256;
-
 
 
 }
